57d36d14faef4f780a3aafe3f2f36982d6b060db
NUR SOLAK <nursolka17@gmail.com>
2025-05-03 13:12:31 +0300

    Update welcome message text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
             textShadow: "1px 1px 2px rgba(0,0,0,0.1)",
           }}
         >
-          Welcome to the
+          Welcome to
         </Typography>
 
         <Typography
